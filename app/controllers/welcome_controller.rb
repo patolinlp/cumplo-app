142819779b95fce427aad49c33d9099fa666d889
patolinlp <patolinlp@hotmail.com>
2018-02-09 00:42:21 -0300

    Vista y obtención de datos de sbif

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,6 @@ class WelcomeController < ApplicationController
   require 'net/http'
 
   def index
-  	uri = URI("http://indicadoresdeldia.cl/webservice/indicadores.json")
-	http = Net::HTTP.get(uri)
-	@parsed = ActiveSupport::JSON.decode(http)
-	
 	
   end
 end
